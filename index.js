9d63cf09915dc07b9cc79fd25c01c1cdcd239351
jypark <babungv@gmail.com>
2021-06-17 21:49:03 +0900

    Default settings for Apollo Server in localhost

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const database = require('./db/database')
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
-
+    type Query {
+        teams: [Team]
+    
+    }
+    type Team {
+        id: Int
+        manager: String
+        office: String
+        extension_number: String
+        mascot: String
+    }
 `
 
 const resolvers = {
-
+    Query: {
+        teams: () => database.teams
+    }
 }
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
